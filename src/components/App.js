e6336cf9ca1b3348c05f917a323ade2fd508024c
Nitin Tembhare <nitintembhare340@gmail.com>
2023-06-24 13:55:01 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,31 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const[value, setValue] = useState({name:"", password:"", error:""})
+
+  const validate = ()=>{
+    if(value.name==="" || value.password===""){
+      setValue({...value, error: "Both username and password are required."})
+      console.log({value})
+    } 
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+        <label htmlFor="name">UserName:</label>
+        <input type="text" onChange={(e)=>setValue({...value, name:e.target.value})}/>
+        <br />
+        <label htmlFor="pass">Password:</label>
+        <input type="password" onChange={(e)=>setValue({...value, password:e.target.value})}/>
+        {
+          value.error && value.error!="" &&
+            <p id="errorMessage">{value.error}</p>
+        }
+        <br />
+        <button type="submit" onClick={validate}>Login</button>
     </div>
   )
 }
