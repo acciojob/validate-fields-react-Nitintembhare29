1e43fda67d4f07f4788723437510179a77b0ea46
Nitin Tembhare <nitintembhare340@gmail.com>
2023-06-24 14:01:37 +0530

    second commit

--- src/components/App.js
+++ src/components/App.js
@@ -14,16 +14,19 @@ const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+        <p>
         <label htmlFor="name">UserName:</label>
         <input type="text" onChange={(e)=>setValue({...value, name:e.target.value})}/>
-        <br />
+        </p>
+        <p>
         <label htmlFor="pass">Password:</label>
         <input type="password" onChange={(e)=>setValue({...value, password:e.target.value})}/>
+        </p>
         {
           value.error && value.error!="" &&
             <p id="errorMessage">{value.error}</p>
         }
-        <br />
+        
         <button type="submit" onClick={validate}>Login</button>
     </div>
   )
